ebd8ca995820601413af193b214082a5f36940a7
Sakshigupta2005 <sakshigupta.02005@gmail.com>
2024-12-13 06:15:37 +0000

    Day 1

---
+++ Day-1/areaofrectangle.py
@@ -0,0 +1,5 @@
+#python program to calculation the area of the Rectangle
+length=float(input("enter a length of the rectangle:"))
+breadth=float(input("enter a breadth of the rectangle:"))
+area=length*breadth
+print(f"area of the rectangle",area)
---
+++ Day-1/oddoreven.py
@@ -0,0 +1,6 @@
+#python program to check a number is odd or even
+num = int(input("Enter a Number:")) 
+if (num % 2) == 0: 
+  print("Given number is Even") 
+else: 
+  print("Given number is Odd")
